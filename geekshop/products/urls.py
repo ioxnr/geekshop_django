65ef4915bca7dcf38dc876aec050dd129a582fa3
ioxnr <lionasalvia@gmail.com>
2021-09-19 16:59:38 +0300

    added json database, base template, changed html files, url files and views

--- geekshop/products/urls.py
+++ geekshop/products/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from products.views import index
+from django.urls import path
+from .views import products
+
+app_name = 'products'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', include('products.urls', namespace='products')),
+    path('', products, name='index'),
 ]
--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+import os, json
+
+MODULE_DIR = os.path.dirname(__file__)
+
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Главная страница'
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    file_path = os.path.join(MODULE_DIR, 'fixtures/db.json')
+    context = {
+        'title': 'geekshop',
+        'products': json.load(open(file_path, encoding='utf-8'))
+    }
+    return render(request, 'products/products.html', context)
