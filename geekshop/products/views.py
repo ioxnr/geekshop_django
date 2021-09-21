071ffb188073dc83f4a7d928fc70521e3ac26656
ioxnr <lionasalvia@gmail.com>
2021-09-21 19:52:19 +0300

    connected to database, setted up admin page, models and changed url files and views

--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import os, json
+from .models import ProductCategory, Product
 
 MODULE_DIR = os.path.dirname(__file__)
 
@@ -15,9 +16,11 @@ def index(request):
 
 
 def products(request):
-    file_path = os.path.join(MODULE_DIR, 'fixtures/db.json')
+    # file_path = os.path.join(MODULE_DIR, 'fixtures/db.json')
+    all_products = Product.objects.all()
     context = {
         'title': 'geekshop',
-        'products': json.load(open(file_path, encoding='utf-8'))
+        'products': all_products
+        # 'products': json.load(open(file_path, encoding='utf-8'))
     }
     return render(request, 'products/products.html', context)
