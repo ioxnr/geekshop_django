6e3a81bb861ba8ffa4931c3334a6360d856d0214
ioxnr <lionasalvia@gmail.com>
2021-10-26 20:02:30 +0300

    added orders app

---
+++ geekshop/orders/urls.py
@@ -0,0 +1,29 @@
+"""geekshop URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from .views import OrderCreate, OrderUpdate, OrderList, OrderDelete, OrderDetail, order_forming_complete
+
+app_name = 'orders'
+
+urlpatterns = [
+    path('', OrderList.as_view(), name='list'),
+    path('create/', OrderCreate.as_view(), name='create'),
+    path('update/<int:pk>/', OrderUpdate.as_view(), name='update'),
+    path('delete/<int:pk>/', OrderDelete.as_view(), name='delete'),
+    path('read/<int:pk>/', OrderDetail.as_view(), name='read'),
+    path('forming_complete/<int:pk>/', order_forming_complete, name='forming_complete'),
+]
