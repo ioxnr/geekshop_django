c452065cd7fddca8fec51afe31013af7ac5e27ee
ioxnr <lionasalvia@gmail.com>
2021-11-05 19:58:40 +0300

    added db scheme and optimization

--- geekshop/orders/forms.py
+++ geekshop/orders/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+
+from products.models import Product
 from .models import Order, OrderItem
 
 
@@ -22,5 +24,6 @@ class OrderItemsForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(OrderItemsForm, self).__init__(*args, **kwargs)
+        self.fields['product'].queryset = Product.objects.all().select_related()
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
