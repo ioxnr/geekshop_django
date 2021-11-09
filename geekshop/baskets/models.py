af9c49abc10db31c7481128425aa889122bc0a2a
ioxnr <lionasalvia@gmail.com>
2021-11-09 16:47:19 +0300

    added caching

--- geekshop/baskets/models.py
+++ geekshop/baskets/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.functional import cached_property
+
 from users.models import User
 from products.models import Product
 
@@ -16,7 +18,7 @@ from products.models import Product
 
 class Basket(models.Model):
     # object = BasketQuerySet.as_manager()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
     create_timestamp = models.DateTimeField(auto_now_add=True)
@@ -29,12 +31,18 @@ class Basket(models.Model):
         return self.quantity * self.product.price
 
     def total_sum(self):
-        baskets = Basket.objects.filter(user=self.user)
+        # baskets = Basket.objects.filter(user=self.user)
+        baskets = self.get_items_cached
         return sum(basket.sum() for basket in baskets)
 
     def total_quantity(self):
-        baskets = Basket.objects.filter(user=self.user)
+        # baskets = Basket.objects.filter(user=self.user)
+        baskets = self.get_items_cached
         return sum(basket.quantity for basket in baskets)
+
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
 
     # def delete(self, using=None, keep_parents=False):
     #     self.product.quantity += self.quantity
